849c77cc83b7144a9e2f9e21001acf69ffd77fc7
DestinyGuilbert <145797311+DestinyGuilbert@users.noreply.github.com>
2025-06-17 17:41:38 -0600

    cambios finales

---
+++ core/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post
+
+def saludar(request):
+    return HttpResponse("Hola desde Django")
+
+def index(request):
+    posts = Post.objects.all()  # obtenemos todos los posts
+    return render(request, 'core/index.html', {'posts': posts})
+
+#def index(request):
+#    return render(request, 'core/index.html')
+#
+
+def index2(request):
+    posts = Post.objects.all()  # ← posts en plural
+    return render(request, 'AppBlog/index.html', {'posts': posts}) 
+
+def saludar_con_etiqueta(request):
+    return HttpResponse('<h1 style="color:red"> Hola </h1>')
+
+def saludar_con_parametros(request, nombre: str, apellido: str):
+    nombre = nombre.capitalize()
+    apellido = apellido.upper()
+    return HttpResponse(f"{apellido}, {nombre}")
+
+def blog(request):
+    posts = Post.objects.all()  # obtenemos todos los posts
+    return render(request, 'core/base.html', {'posts': posts})
